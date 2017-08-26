7a7e3da15aff7b53498b7ad3ebfd849b3ff50045
bincool <chengyiwang9303@foxmail.com>
2017-08-26 20:32:43 +0800

    目录结构

---
+++ src/com/wchy/structure/common/TimeSn.java
@@ -0,0 +1,55 @@
+package com.wchy.structure.common;
+
+/**
+ * 单例类-生产递增种子.
+ * @author bingV
+ *
+ */
+public class TimeSn 
+{
+	
+	/**
+	 * 单例生产递增种子对象.
+	 */
+	private static final TimeSn instance = new TimeSn();
+	
+	/**
+	 * 递增种子.
+	 */
+	private static long sn = 1000;
+	
+	/**
+	 * 中间变量，减少对象的声明.
+	 */
+	private static final StringBuffer sb = new StringBuffer();
+	
+	/**
+	 * 获取单例生产递增种子对象.
+	 * @return
+	 */
+	public static TimeSn getInstance() 
+	{
+		return instance;
+	}
+	
+	/**
+	 * 获取递增种子.
+	 * @return
+	 */
+	public long getSn() 
+	{
+		sb.delete(0, sb.length());
+		sb.append(System.currentTimeMillis());
+		sb.append(sn);
+		if (sn == 10000) 
+		{
+			sn = 1000;
+		} 
+		else 
+		{
+			sn++;
+		}
+		return Long.parseLong(sb.toString());
+	}
+
+}
